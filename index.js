dfbe5dfa2ec5be6a1c8fb416bbf77f6e23f74fdf
Felipe S. Oliver <85701952+felipesantosdd@users.noreply.github.com>
2022-09-27 18:24:31 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function Imprimir(dados) {
-const doc = new jsPDF('retrato', 'mm', [597,410])
-doc.setFont('helvetica')
-doc.setFontStyle('bold')
-doc.text("Produto:")
-}
-
-const dados = document.getElementById(Produto)
-console.log(dados);
